364df0ed90e4f91330eb9c2c93a8da5526aff205
golovko1985 <golovko1985@gmail.com>
2021-09-18 22:55:18 +0300

    Add CodeWars "Your order, please" task https://www.codewars.com/kata/55c45be3b2079eccff00010f

---
+++ CodeWarsYourOrderPlease/Kata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace CodeWarsYourOrderPlease
+{
+    public static class Kata
+    {
+        public static string Order(string words)
+        {
+            char[] separators = new char[] { ' ', '.' };
+            string[] subs = words.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            string[] sortStr = new string[subs.Length];
+            foreach (string s in subs)
+            {
+                char ch = s.Where(x => Char.IsDigit(x)).First();
+                int i = ch - '1';
+                sortStr[i] = s;
+            }
+            StringBuilder sb = new StringBuilder(words.Length);
+            sb.AppendJoin(' ', sortStr);
+            return sb.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Kata.Order("is2 Thi1s T4est 3a"));
+            Console.WriteLine("expexted: Thi1s is2 3a T4est");
+        }
+    }
+}
---
+++ CodeWarsYourOrderPleaseNTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using CodeWarsYourOrderPlease;
+using NUnit.Framework;
+
+namespace CodeWarsYourOrderPleaseNTests
+{
+    public class Tests
+    {
+        [Test, Description("Sample Tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual("", Kata.Order(""));
+            Assert.AreEqual("Thi1s is2 3a T4est", Kata.Order("is2 Thi1s T4est 3a"));
+            Assert.AreEqual("Fo1r the2 g3ood 4of th5e pe6ople", Kata.Order("4of Fo1r pe6ople g3ood th5e the2"));
+
+        }
+    }
+}
