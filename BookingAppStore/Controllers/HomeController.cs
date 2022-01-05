af4b058d979f983ce673e8f1e36c3e6d367a453d
Golovko_AA <Golovko_AA@usoft.ru>
2022-01-05 21:31:12 +0300

    BookingAppStore init

---
+++ BookingAppStore/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using BookingAppStore.Models;
+using BookingAppStore.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookingAppStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        // создаем контекст данных
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            // получаем из бд все объекты Book
+            var books = db.Books;
+            // передаем все объекты в динамическое свойство Books в ViewBag
+            ViewBag.Books = books;
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+        public string GetId(int Id)
+        {
+            return Id.ToString();
+        }
+
+        public string Square()
+        {
+            int a = Int32.Parse(Request.Params["a"]);
+            int h = Int32.Parse(Request.Params["h"]);
+            double s = a * h / 2.0;
+            return "<h2>Площадь треугольника с основанием " + a +
+                    " и высотой " + h + " равна " + s + "</h2>";
+        }
+
+        public ActionResult GetHtml()
+        {
+            return new HtmlResult("<h2>Привет мир!</h2>");
+        }
+
+        public ActionResult GetImage()
+        {
+            string path = "../Content/Images/uninstaller.png";
+            return new ImageResult(path);
+        }
+
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            //string title = Request.Form["title"];
+
+            purchase.Date = DateTime.Now;
+            // добавляем информацию о покупке в базу данных
+            db.Purchases.Add(purchase);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо," + purchase.Person + ", за покупку!";
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ HelloMVCApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloMVCApp.Startup))]
+namespace HelloMVCApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
